b65fbc1b81ff6cacc8a6f677722e5b34624158a0
coderShivam45 <shivamsingh45612360@gmail.com>
2024-08-26 00:51:57 +0530

    Tshirt page updated

--- src/views/TshirtBanner.vue
+++ src/views/TshirtBanner.vue
@@ -5,25 +5,35 @@
             <div class="merch">OFFICAL TED<span>X</span><br />MERCHANDISE</div>
         </div>
         <div class="tshirt">
-            <div class="combo">
+            <div class="combo-container">
+                <div class="combo">
+                    <div>
+                        <h2 class="combo-offer">COMBO OFFER</h2>
+                        <h3 class="off">10% off on 3 T-shirt</h3>
+                    </div>
+
+
+                    <div class="price">
+                        <h3 class="old-price">₹1107 </h3>
+                        <h2 class="offer-price">₹1079</h2>
+                        <h3 class="buy">buy now</h3>
+                    </div>
+
+                    <div class="tshirtgroup">
+                        <img src="@/assets/images/tshirtgroup.png" alt="">
+                    </div>
+                </div>
                 <div>
-                    <h2 class="combo-offer">COMBO OFFER</h2>
-                    <h3 class="off">10% off on 3 T-shirt</h3>
-                </div>
-
-
-                <div class="price">
-                    <h3 class="old-price">₹1107 </h3>
-                    <h2 class="offer-price">₹1079</h2>
-                    <h3 class="buy">buy now</h3>
-                </div>
-
-                <div class="tshirtgroup">
-                    <img src="@/assets/images/tshirtgroup.png" alt="">
+                    <button class="buttonbuy">BUY NOW </button>
                 </div>
             </div>
-            <div class="single">
-                <img src="@/assets/images/tshirtsingle.png" alt="">
+            <div class="single-container">
+                <div class="single">
+                    <img src="@/assets/images/tshirtsingle.png" alt="">
+                </div>
+                <div>
+                    <button class="buttonbuy">BUY NOW </button>
+                </div>
             </div>
         </div>
     </div>
@@ -36,5 +46,5 @@ export default {
 </script>
 
 <style>
- @import '../assets/css/tshirtbanner.css';
+@import '../assets/css/tshirtbanner.css';
 </style>
