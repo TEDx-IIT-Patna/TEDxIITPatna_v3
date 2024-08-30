7a44de61d18e050b82f03af61ff1d94b15422825
Ryujin <ryujinunofficial01@gmail.com>
2024-08-30 16:37:39 +0530

    Resolved the merge conflict and made the pages responsive

---
+++ src/components/NewNav.vue
@@ -0,0 +1,50 @@
+<template>
+  <span
+    class="show-panel"
+    style="
+      color: white;
+      background-color: transparent;
+      transform: translate(-50%, 0px);
+      opacity: 1;
+    "
+  >
+    <i class="gg-menu-left-alt" @click="navOpen = true"></i>
+  </span>
+
+  <nav class="nav" :class="{ hidden: !navOpen, show: navOpen }">
+    <div class="close" @click="navOpen = false">Close</div>
+    <div class="links">
+      <NavLinks />
+      <router-link to="/merch" class="buy">Buy</router-link>
+    </div>
+    <img
+      src="../assets/images/Flower-cropped.png"
+      class="flower"
+      style="opacity: 0.7"
+    />
+  </nav>
+</template>
+
+<script>
+import NavLinks from "./NavLinks.vue";
+
+export default {
+  components: {
+    NavLinks,
+  },
+
+  data() {
+    return {
+      navOpen: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "../assets/css/newNav.css";
+
+.links .buy {
+  color: #eb0028;
+}
+</style>
--- src/components/SpeakerDetailModal.vue
+++ src/components/SpeakerDetailModal.vue
@@ -46,78 +46,85 @@
 -->
 
 <template>
-    <div :class="['speaker-modal-wrapper', 0.9 * this.screenWidth >= this.screenHeight ? 'web' : 'mobile']"
-        v-if="this.showModal">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="close-btn" @click="this.$emit('closeSpeakerModal')"><i class="gg-close"></i></div>
-            <img :src="imageSrc" class="pic">
-            <div class="content">
-                <div class="name-wrapper">
-                    <div class="name">{{ name }}</div>
-                    <div class="title">{{ title }}</div>
-                </div>
-                <div class="desc">
-                    <span v-html="content"></span>
-                    <a :href="talkLink" target="_blank" class="talk" v-if="talkLink">Talk</a>
-                </div>
-            </div>
+  <div
+    :class="[
+      'speaker-modal-wrapper',
+      0.9 * this.screenWidth >= this.screenHeight ? 'web' : 'mobile',
+    ]"
+    v-if="this.showModal"
+  >
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="close-btn" @click="this.$emit('closeSpeakerModal')">
+        <i class="gg-close"></i>
+      </div>
+      <img :src="imageSrc" class="pic" />
+      <div class="content">
+        <div class="name-wrapper">
+          <div class="name">{{ name }}</div>
+          <div class="title">{{ title }}</div>
         </div>
+        <div class="desc">
+          <span v-html="content"></span>
+          <a :href="talkLink" target="_blank" class="talk" v-if="talkLink"
+            >Talk</a
+          >
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'GallerySpeakerModal',
-    props: {
-        'imageSrc': {
-            type: String
-        },
-        'name': {
-            type: String
-        },
-        'title': {
-            type: String
-        },
-        'content': {
-            type: String
-        },
-        'showModal': {
-            type: Boolean,
-            default: false
-        },
-        'talkLink': {
-            type: String,
-            default: ""
-        }
+  name: "GallerySpeakerModal",
+  props: {
+    imageSrc: {
+      type: String,
     },
-    emits: [
-        'closeSpeakerModal'
-    ],
-    data() {
-        return {
-            screenWidth: window.innerWidth,
-            screenHeight: window.innerHeight,
-        }
+    name: {
+      type: String,
     },
-    methods: {
-        onResize() {
-            this.screenWidth = window.innerWidth
-            this.screenHeight = window.innerHeight
-        },
+    title: {
+      type: String,
     },
-    mounted() {
-        this.$nextTick(() => {
-            window.addEventListener('resize', this.onResize)
-        })
+    content: {
+      type: String,
     },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize)
-    }
-}
+    showModal: {
+      type: Boolean,
+      default: false,
+    },
+    talkLink: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["closeSpeakerModal"],
+  data() {
+    return {
+      screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight,
+    };
+  },
+  methods: {
+    onResize() {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    },
+  },
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  },
+};
 </script>
 
 <style scoped>
-@import '@/assets/css/speaker_detail_modal.css';
-@import '@/assets/css/speaker_detail_modal.mobile.css';
+@import "@/assets/css/speaker_detail_modal.css";
+@import "@/assets/css/speaker_detail_modal.mobile.css";
 </style>
